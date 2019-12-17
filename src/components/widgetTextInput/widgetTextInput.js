8d965ed4ec7056721ca5ccf17f4ea0fbb5e789e5
sarahduv <sarah.duvdevani@gmail.com>
2019-12-17 15:41:23 -0800

    adding widget work from tuesday

--- src/components/widgetTextInput/widgetTextInput.js
+++ src/components/widgetTextInput/widgetTextInput.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import './widgetTextInput.scss';
 import appEvents from '../../appEvents.js';
+import $ from 'jquery';
 
 class WidgetPicker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dummy: []
+      geoData: []
     };
   }
 
-  // handleGetGeoData = () => {
-  //   //TODO: make this configurable
-  //   $.ajax({
-  //     url: 'http://localhost:3333/api/getGeoData'
-  //   }).done(result => {
-  //     this.setState({ series: result.series, isLoading: false });
-  //   });
-  // };
+  handleKeyDown = e => {
+    if (e.key === 'Enter') {
+      appEvents.onTextInputEntered(e.target.value);
+      // fetchGeoData(e.target.value);
+    }
+  };
 
-  // handleKeyDown = e => {
-  //   if (e.key === 'Enter') {
-  //     appEvents.onTextInputEntered(e.target.value);
-  //     console.log('this is e.target.value', e.target.value);
-  //   }
-  // };
 
   render() {
     return (
